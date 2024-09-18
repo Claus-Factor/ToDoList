73276f955ebe513b917e66aa2eecce8f4ffacdd0
Nicholas527 <volxin.k2004@gmail.com>
2024-09-19 00:51:51 +0300

    Connect the localStorage for saving added list-items

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
 const todo__list = document.querySelector(".todo__list")
+const btn = document.querySelector(".todo__input button")
+
+let myStore = window.localStorage;
+if (myStore.length===0) {
+    myStore.setItem(0,"[]");
+}
+let arr = JSON.parse(myStore.getItem(0));
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== null) {
+        let todo__item = document.createElement("li");
+        let todo__li = document.createElement("span");
+        let todo__delete = document.createElement("div");
+        todo__li.textContent = arr[i];
+        todo__delete.textContent = "X";
+        todo__item.className = "todo__item";
+        todo__li.className = "todo__li";
+        todo__delete.className = "todo__delete";
+        todo__list.appendChild(todo__item);
+        todo__item.appendChild(todo__li);
+        todo__item.appendChild(todo__delete);
+
+        todo__delete.onclick = () => {
+            let textToDel = todo__li.textContent;
+            let index = arr.indexOf(textToDel);
+            delete arr[index];
+            myStore.setItem(0,JSON.stringify(arr));
+
+            todo__delete.parentElement.style.display = "none"
+        }
+    }
+}
 todo__list.addEventListener("click", (ev) => {
     ev.target.classList.toggle("completed")
 })
 
-const btn = document.querySelector(".todo__input button")
+
 btn.onclick = () => {
     if (document.querySelector(".todo__input input").value !== "") {
         let todo__item = document.createElement("li");
@@ -18,6 +50,18 @@ btn.onclick = () => {
         todo__list.appendChild(todo__item);
         todo__item.appendChild(todo__li);
         todo__item.appendChild(todo__delete);
+
+        arr.push(todo__li.textContent);
+        myStore.setItem(0,JSON.stringify(arr));
+
+        todo__delete.onclick = () => {
+            let textToDel = todo__li.textContent;
+            let index = arr.indexOf(textToDel);
+            delete arr[index];
+            myStore.setItem(0,JSON.stringify(arr));
+
+            todo__delete.parentElement.style.display = "none"
+        }
     } else {
         alert("Введите какую-либо задачу!");
     }
