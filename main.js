382cae2773c49046064f750465e344cd2173ee23
Nicholas527 <volxin.k2004@gmail.com>
2024-09-18 23:27:11 +0300

    Make the ability to add list-items

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-const todo__list = document.querySelector(".todo__list>ul")
+const todo__list = document.querySelector(".todo__list")
 todo__list.addEventListener("click", (ev) => {
     ev.target.classList.toggle("completed")
 })
+
+const btn = document.querySelector(".todo__input button")
+btn.onclick = () => {
+    if (document.querySelector(".todo__input input").value !== "") {
+        let todo__item = document.createElement("li");
+        let todo__li = document.createElement("span");
+        let todo__delete = document.createElement("div");
+        todo__li.textContent = document.querySelector(".todo__input input").value;
+        document.querySelector(".todo__input input").value = "";
+        todo__delete.textContent = "X";
+        todo__item.className = "todo__item";
+        todo__li.className = "todo__li";
+        todo__delete.className = "todo__delete";
+        todo__list.appendChild(todo__item);
+        todo__item.appendChild(todo__li);
+        todo__item.appendChild(todo__delete);
+    } else {
+        alert("Введите какую-либо задачу!");
+    }
+}
